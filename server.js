6ba86becb3fd5de01448dd9fef82d5cc5dba5ed4
arai <niimaru09@gmail.com>
2023-11-13 21:32:19 +0900

    connect to abe hiroshi

--- server.js
+++ server.js
@@ -16,4 +16,12 @@ app.get("/", (req, res) => {
   res.render("index", { text: "こんにちは" });
 });
 
+app.get("source/menu.html", (req, res) => {
+    res.render("/source/menu.html");
+  });
+
+app.get("/source/top.html", (req, res) => {
+    res.sendFile(__dirname + "/views/source/top.html");
+  });
+
 app.listen(3000, console.log("サーバーが起動しました"));
