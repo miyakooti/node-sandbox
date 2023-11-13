5764bc0250cb9fcb31ca287cee98356867b89076
arai <niimaru09@gmail.com>
2023-11-13 19:05:33 +0900

    build server with express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+// app.use(logger);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  console.log("Hello Express");
+  //res.send("<h1>Hello NodeJs</h1>");
+  //res.sendStatus(500);
+  //res.status(500).send("Error");
+  //res.status(500).json({ msg: "エラー" });
+  res.render("index", { text: "こんにちは" });
+});
+
+app.listen(3000, console.log("サーバーが起動しました"));
