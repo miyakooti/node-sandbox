c257cd56ab9cd412cab443afb9fddad6616d53a5
arai <niimaru09@gmail.com>
2023-11-15 12:58:17 +0900

    fix mojibake and source path

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get("/", (req, res) => {
   res.render("index", { text: "こんにちは" });
 });
 
-app.get("source/menu.html", (req, res) => {
-    res.render("/source/menu.html");
+app.get("/source/menu.html", (req, res) => {
+    res.sendFile(__dirname + "/views/source/menu.html");
   });
 
 app.get("/source/top.html", (req, res) => {
