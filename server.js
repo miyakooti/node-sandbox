8c0460fef15db528f62fa4bbbbffb1b0bf9932d1
arai <niimaru09@gmail.com>
2023-11-18 11:50:46 +0900

    api投げてimageurlパースしてそれを表示 htmlとejs両方用意することで解決した。ぶっちゃけクソ疲れた。

--- server.js
+++ server.js
@@ -15,31 +15,7 @@ app.set("view engine", "ejs");
 
 app.get('/', async (req, res) => {
 
-    console.log('started root path')
 
-
-    var request = require("request");
-    var fs = require("fs");
-
-    var image_file = fs.createReadStream(path.join(__dirname, 'uploads/1700030491687.jpg'))
-
-    console.log(image_file)
-
-    request({
-        method: "POST",
-        url: "https://techhk.aoscdn.com/api/tasks/visual/segmentation",
-        headers: {
-        "X-API-KEY": "wx9sjlg1796km3kfm"
-        },
-        formData: {
-        sync: "1",
-        image_file: image_file,
-        }
-    }, function (error, response) {
-        console.log('function now')
-        if (error) throw new Error(error);
-        console.log(response.body);
-    });
 
 
 
@@ -47,38 +23,15 @@ app.get('/', async (req, res) => {
 
 });
 
-app.get('/uploads/1700030491687.jpg', (req, res) => {
-    const imagePath = path.join(__dirname, 'uploads', '1700030491687.jpg');
-    res.sendFile(imagePath);
-    res.setHeader('Content-Type', 'image/jpeg');
-  });
-
-async function makeApiRequest() {
-    const imagePath = path.join(__dirname, 'views', 'source', 'ore.jpg');
-    const imageContent = fs.readFileSync(imagePath);
-    const API_KEY = 'wx9sjlg1796km3kfm';
-
-    // axiosを使用して非同期にAPIリクエストを行う
-    const response = await axios.post('/uploads/1700030491687.jpg', imageContent, {
-        headers: {
-            'Content-Type': 'image/jpeg',
-            'X-API-KEY': API_KEY,
-        },
-        params: {
-            sync: '1',
-        },
-    });
-
-    return response.data;
-}
 
 app.get("/source/menu.html", (req, res) => {
     res.sendFile(__dirname + "/views/source/menu.html");
   });
 
-app.get("/source/top.html", (req, res) => {
-    res.sendFile(__dirname + "/views/source/top.html");
+app.get("/top.html", (req, res) => {
+    res.sendFile(__dirname + "/views/top.html");
   });
+  
 
 app.listen(3000, console.log("サーバーが起動しました"));
 
@@ -107,7 +60,51 @@ const upload = multer({ storage });
 // アップロードされた画像を保存するエンドポイント
 app.post('/upload', upload.single('image'), (req, res) => {
     // ファイルがアップロードされた後の処理
-    res.send('File uploaded!');
+    console.log('started uploading path')
+
+    const textData = req.body.text; // テキストデータはreq.body.textでアクセスできる
+
+    console.log(textData);
+
+
+
+    var request = require("request");
+    var fs = require("fs");
+
+    var image_file = fs.createReadStream(path.join(__dirname, 'uploads/for-abe-hiroshi.jpg'))
+
+    console.log(image_file)
+
+    request({
+        method: "POST",
+        url: "https://techhk.aoscdn.com/api/tasks/visual/segmentation",
+        headers: {
+        "X-API-KEY": "wx9sjlg1796km3kfm"
+        },
+        formData: {
+        sync: "1",
+        image_file: image_file,
+        }
+    }, function (error, response) {
+        console.log('function now')
+        if (error) throw new Error(error);
+
+        // JSON文字列をJavaScriptオブジェクトに変換
+        const responseObj = JSON.parse(response.body);
+
+        // "image" プロパティの値を取得
+        const imageUrl = responseObj.data.image;
+
+        console.log(imageUrl);
+
+        // res.render('top', { imageUrl: imageUrl, name_jap: textData });
+        res.render('top', { name: textData, imageUrl: imageUrl });
+
+    });
+
+
+
+
 });
 
 
